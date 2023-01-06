751bef28ae11ab7acab8c5351699455628b58f4b
Alex Scarborough <alex@teak.io>
2023-01-06 12:29:42 -0800

    Add deployomat slack messages.

---
+++ src/scripts/slack_deployomat_cancel_on_hold_message.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Copyright 2023 Teak.io, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SetupEnv() {
+  I_TEMPLATE_NAME=$(eval echo "${I_TEMPLATE_NAME}")
+  I_SERVICE_NAME=$(eval echo "${I_SERVICE_NAME}")
+
+  echo "I_TEMPLATE_NAME"="${I_TEMPLATE_NAME}"
+  echo "I_SERVICE_NAME"="${I_SERVICE_NAME}"
+}
+
+BuildMessage() {
+  if [ -n "$I_SERVICE_NAME" ]; then
+    SERVICE_ADDON=" (service: ${I_SERVICE_NAME})"
+  fi
+  HEADER="${CIRCLE_PROJECT_REPONAME} ${CIRCLE_BRANCH}${SERVICE_ADDON} is deploying, go here to cancel."
+  mkdir -p /tmp/teak-orb
+  echo "{
+      \"text\": \"${HEADER}\",
+      \"blocks\": [
+        {
+          \"type\": \"header\",
+          \"text\": {
+            \"type\": \"plain_text\",
+            \"text\": \"${HEADER} :octagonal_sign::no_good: \",
+            \"emoji\": true
+          }
+        },
+        {
+          \"type\": \"section\",
+          \"fields\": [
+            {
+              \"type\": \"mrkdwn\",
+              \"text\": \"<https://app.circleci.com/pipelines/workflows/${CIRCLE_WORKFLOW_ID}|View Workflow>\"
+            }
+          ]
+        }
+      ]
+    }" > /tmp/teak-orb/deploy-cancel-slack-template.json
+    echo "export ${I_TEMPLATE_NAME}=\$(cat /tmp/teak-orb/deploy-cancel-slack-template.json)" >> "${BASH_ENV}"
+}
+
+SetupEnv
+BuildMessage
